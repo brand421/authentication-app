e984daaa9b4e9b9d39669c5017910ba609366d3b
brand421 <57531511+brand421@users.noreply.github.com>
2023-05-23 13:14:17 -0400

    add register new account functionality

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require ('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended:true }));
+
+const uri = require(__dirname + "/mongodburl.js");
+const url = Object.values(uri).toString();
+
+mongoose.connect(url, { useNewUrlParser: true });
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res) {
     res.render("home");
@@ -18,6 +31,15 @@ app.get("/login", function(req, res) {
 
 app.get("/register", function(req, res) {
     res.render("register");
+})
+
+app.post("/register", function(req, res) {
+    const newUser = new User ({
+        email: req.body.username,
+        password: req.body.password
+    });
+    
+    newUser.save();
 })
 
 app.listen(3000, function() {
