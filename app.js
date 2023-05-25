0cd50ff41bfec59b13bcde7f3c569896af2d35bc
brand421 <57531511+brand421@users.noreply.github.com>
2023-05-25 13:06:44 -0400

    swap key encryption with md5 hash encryption

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require ('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 
 const app = express();
@@ -22,8 +22,6 @@ const userSchema = new mongoose.Schema ({
     password: String
 })
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET_KEY, encryptedFields: ["password"] });
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res) {
@@ -41,7 +39,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
     const newUser = new User ({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     
     newUser.save();
@@ -50,7 +48,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username})
         .then(function(results) {
