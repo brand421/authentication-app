9f6585ef28ad5f311cd043938479fcc7d771d447
brand421 <57531511+brand421@users.noreply.github.com>
2023-05-25 17:02:55 -0400

    add bcrypt and password salting

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require ('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 
 const app = express();
@@ -37,27 +39,34 @@ app.get("/register", function(req, res) {
 })
 
 app.post("/register", function(req, res) {
-    const newUser = new User ({
-        email: req.body.username,
-        password: md5(req.body.password)
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User ({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save();
+        res.render("secrets");
     });
     
-    newUser.save();
-    res.render("secrets");
 })
 
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username})
         .then(function(results) {
             if(!results) {
                 console.error();
             } else {
-                if(results.password === password) {
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, results.password, function(err, result) {
+                    if(result === true) {
+                        res.render("secrets");
+                    }
+                });
+                
             }
         })
 })
