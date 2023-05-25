4ec64ba06d8ef10188bd31d2bf54dca66cefa06d
brand421 <57531511+brand421@users.noreply.github.com>
2023-05-25 11:28:02 -0400

    add env file and integrate with node file

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require ('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
+
 
 const app = express();
 
@@ -20,8 +22,7 @@ const userSchema = new mongoose.Schema ({
     password: String
 })
 
-const secret = "T******************n";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET_KEY, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
